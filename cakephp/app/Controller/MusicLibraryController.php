82e8cac2c265353bbac985540b49005645a95e9e
Eric Freese <ericdfreese@gmail.com>
2012-05-24 01:16:30 -0500

    Add support for quoted keyword searches like: '"the beatles" nine'

--- cakephp/app/Controller/MusicLibraryController.php
+++ cakephp/app/Controller/MusicLibraryController.php
@@ -18,13 +18,14 @@ class MusicLibraryController extends AppController {
 		$conditions = array();
 		
 		if (isset($this->request->query['q'])) {
-			$q = split(' ', $this->request->query['q']);
+			preg_match_all('/"[^"]+"|\w+/', $this->request->query['q'], $q);
 			
 			$conditions = array(
 				'AND' => array()
 			);
 			
-			for ($i = 0; $i < count($q); $i++) { 
+			for ($i = 0; $i < count($q); $i++) {
+				$q = str_replace('"', '', $q[$i]);
 				array_push($conditions['AND'], array('OR' => array(
 					'Album.a_Title LIKE' => '%'.$q[$i].'%',
 					'Album.a_Artist LIKE' => '%'.$q[$i].'%',
